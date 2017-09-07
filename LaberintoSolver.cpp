f58280acb9df3a2e94cbdad3792aaf36cf4214dd
NNelo <nelogino@gmail.com>
2017-09-07 13:34:49 -0300

    laberinto con pilas

--- LaberintoSolver.cpp
+++ LaberintoSolver.cpp
@@ -1,5 +1,10 @@
 #include "LaberintoSolver.h"
+#include "Pila/Pila.h"
 
+typedef struct {
+    unsigned x;
+    unsigned y;
+} coord;
 
 /**
  * Función recursiva para solucionar el laberinto
@@ -8,10 +13,49 @@
  * @return true si la celda es parte de la solución, sino false
  */
 bool LaberintoSolver::solve(unsigned x, unsigned y) {
-    m->visitar(x, y);
-    callback(x, y, 1);
-    callback(x, y, 0); //Encontre
-    return false;
+    Pila<coord> pila;
+    coord actual;
+    bool termine = false;
+
+    actual.x = x;
+    actual.y = y;
+
+    pila.push(actual);
+
+    while (!pila.esVacia()) {
+        actual = pila.peek();
+
+        x = actual.x;
+        y = actual.y;
+        if (!m->esVisitado(x, y)) {
+            m->visitar(x, y);
+            callback(x, y, 1);
+        }
+
+        int celda = m->getData(x, y);
+
+        //base
+        if ((x == w - 1 && y == h - 1) || termine) {
+            callback(x, y, 2); //econtre
+            termine = true;
+            pila.pop();
+        }
+
+        //Visito ajoba
+        else if ((celda & 0x02) && y != h - 1 && !m->esVisitado(x, y + 1))
+            pila.push({x, y + 1});
+        else if ((celda & 0x04) && x != w - 1 && !m->esVisitado(x + 1, y))
+            pila.push({x + 1, y});
+        else if ((celda & 0x01) && y != 0 && !m->esVisitado(x, y - 1))
+            pila.push({x, y - 1});
+        else if ((celda & 0x08) && x != 0 && !m->esVisitado(x - 1, y))
+            pila.push({x - 1, y});
+        else {
+            callback(x, y, 0);
+            pila.pop();
+        }
+    }
+
 }
 
 /**
